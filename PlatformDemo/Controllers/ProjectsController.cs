ce15a7ba147e8c23b008ef9ba1c966b6656f5f4e
Vinicius Martins <viniciusmv91@gmail.com>
2021-11-10 23:36:01 -0300

    Web API Routing #3

---
+++ PlatformDemo/Controllers/ProjectsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformDemo.Controllers
+{
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/[controller]")]
+        public IActionResult Get()
+        {
+            return Ok("Reading all the projects");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Reading project #{id}");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Creating  a project");
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok("Updating a project");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete()
+        {
+            return Ok("Deleting a project #{id}");
+        }
+    }
+}
