1855d49a4a6542d652d91121f11e5a58822e9d75
Vinicius Martins <viniciusmv91@gmail.com>
2021-11-10 23:23:02 -0300

    Web API Routing #1

---
+++ PlatformDemo/Controllers/TicketsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformDemo.Controllers
+{
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/tickets")]
+        public IActionResult Get()
+        {
+            return Ok("Reading all the tickets");
+        }
+
+        [HttpGet]
+        [Route("api/ticket/{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"Reading ticket #{id}");
+        }
+
+        [HttpPost]
+        [Route("api/tickets")]
+        public IActionResult Post()
+        {
+            return Ok("Creating  a ticket");
+        }
+
+        [HttpPut]
+        [Route("api/tickets")]
+        public IActionResult Put()
+        {
+            return Ok("Updating a ticket");
+        }
+
+        [HttpDelete]
+        [Route("api/ticket/{id}")]
+        public IActionResult Delete()
+        {
+            return Ok("Deleting a ticket #{id}");
+        }
+    }
+}
