c8cc779560065f359c79775e0b0fcea0ef8a7bc5
Vinicius Martins <viniciusmv91@gmail.com>
2021-11-11 23:45:01 -0300

    Model Validation with Data Annotation

--- PlatformDemo/Models/Ticket.cs
+++ PlatformDemo/Models/Ticket.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,31 @@ namespace PlatformDemo.Models
 {
     public class Ticket
     {
-        public int TicketId { get; set; }
-        public int ProjectId { get; set; }
+        public int? TicketId { get; set; }
+
+        [Required]
+        public int? ProjectId { get; set; }
+
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Range(0, 10_000)]
+        public double Price { get; set; }
+
+        [StringLength(100)]
+        public string Address { get; set; }
+
+        [MaxLength(64_000)]
+        public byte[] File { get; set; }
+
+        [CreditCard]
+        public string CreditCard { get; set; }
+
+        [Phone]
+        public string  PhoneNumber { get; set; }
     }
 }
