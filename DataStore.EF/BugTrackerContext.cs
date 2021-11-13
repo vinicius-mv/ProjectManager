9c86474dbbc3131ada3db1df3d0026a258c580c8
Vinicius Martins <viniciusmv91@gmail.com>
2021-11-13 18:37:36 -0300

    Add Context and EntityFramework - Set up In-Memory EF Core for development enviroment

---
+++ DataStore.EF/BugTrackerContext.cs
@@ -0,0 +1,39 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStore.EF
+{
+    public class BugTrackerContext : DbContext
+    {
+        public BugTrackerContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Relation ProjectTickets (1 - n)
+            modelBuilder.Entity<Project>()
+                .HasMany(p => p.Tickets)
+                .WithOne(t => t.Project)
+                .HasForeignKey(t => t.ProjectId);
+
+            // Seeding
+            modelBuilder.Entity<Project>().HasData(
+                new Project { ProjectId = 1, Name = "Project 1" },
+                new Project { ProjectId = 2, Name = "Project 2" });
+
+            modelBuilder.Entity<Ticket>().HasData(
+                new Ticket { TicketId = 1, Title = "Bug #1", ProjectId = 1 },
+                new Ticket { TicketId = 2, Title = "Bug #2", ProjectId = 1 },
+                new Ticket { TicketId = 3, Title = "Bug #3", ProjectId = 2 });
+        }
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataStore.EF;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,20 +14,38 @@ namespace PlatformDemo
 {
     public class Startup
     {
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            // applying resource filter globally
+            if(_env.IsDevelopment())
+            {
+                services.AddDbContext<BugTrackerContext>(options =>
+                {
+                    options.UseInMemoryDatabase("BugTracker");
+                });
+            }
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, BugTrackerContext context)
         {
-            if (env.IsDevelopment())
+            if (_env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                // Create in-memory database for dev enviroment
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
             }
 
             app.UseRouting();
