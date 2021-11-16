7480354e9786be98836c74869085668f1b912b7f
Vinicius Martins <viniciusmv91@gmail.com>
2021-11-15 23:44:15 -0300

    Web API Executer Helper Class

---
+++ App.Repository/ApiClient/WebApiExecutor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repository.ApiClient
+{
+    public class WebApiExecutor
+    {
+        private readonly string _baseUrl;
+        private readonly HttpClient _httpClient;
+
+        public WebApiExecutor(string baseUrl, HttpClient httpClient)
+        {
+            _baseUrl = baseUrl;
+            _httpClient = httpClient;
+
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<T> Invoke<T>(string uri)
+        {
+            return await _httpClient.GetFromJsonAsync<T>(GetUrl(uri));
+        }
+
+        public async Task<T> InvokePost<T>(string uri, T obj)
+        {
+            var response = await _httpClient.PostAsJsonAsync<T>(GetUrl(uri), obj);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<T>();
+        }
+
+        public async Task InvokePut<T>(string uri, T obj)
+        {
+            var response = await _httpClient.PutAsJsonAsync<T>(GetUrl(uri), obj);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task InvokeDelete<T>(string uri)
+        {
+            var response = await _httpClient.DeleteAsync(GetUrl(uri));
+            response.EnsureSuccessStatusCode();
+        }
+
+        private string GetUrl(string uri)
+        {
+            return $"{_baseUrl}/{uri}";
+        }
+    }
+}
