a43d88fe3b6d1296dac95cf3f2e5ddd5fc03b5a5
Vinicius Martins <viniciusmv91@gmail.com>
2021-11-21 15:05:20 -0300

    View Projects Use Case & Code Organization

---
+++ App.ApplicationLogic/ProjectsScreenUseCases.cs
@@ -0,0 +1,24 @@
+﻿using App.Repository;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace App.ApplicationLogic
+{
+    // it's common to call this layer Service, so this class would call ProjectsService
+    public class ProjectsScreenUseCases
+    {
+        private readonly IProjectRepository _projectRepository;
+
+        public ProjectsScreenUseCases(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<IEnumerable<Project>> ViewProjects()
+        {
+            return await _projectRepository.GetAsync();
+        }
+    }
+}
