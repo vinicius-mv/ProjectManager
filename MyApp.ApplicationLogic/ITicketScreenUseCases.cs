7835d3b30668d0bbfbd9aca2259c6ac850ad2f83
Vinicius Martins <viniciusmv91@gmail.com>
2021-11-24 23:42:15 -0300

    Delete Ticket Use Case

--- MyApp.ApplicationLogic/ITicketScreenUseCases.cs
+++ MyApp.ApplicationLogic/ITicketScreenUseCases.cs
@@ -6,6 +6,7 @@ namespace MyApp.ApplicationLogic
     public interface ITicketScreenUseCases
     {
         Task<int> AddTicketAsync(Ticket ticket);
+        Task DeleteTicketAsync(int ticketId);
         Task UpdateTicketAsync(Ticket ticket);
         Task<Ticket> ViewTicketByIdAsync(int ticketId);
     }
