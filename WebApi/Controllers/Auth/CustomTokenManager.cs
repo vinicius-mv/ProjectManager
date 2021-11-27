ae00c22ebb1a5f16726d355ae713b92be5970c7f
Vinicius Martins <viniciusmv91@gmail.com>
2021-11-27 10:26:11 -0300

    Implement Token Manager and User Manager

--- WebApi/Controllers/Auth/CustomTokenManager.cs
+++ WebApi/Controllers/Auth/CustomTokenManager.cs
@@ -1,19 +1,32 @@
-﻿namespace WebApi.Controllers.Auth
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApi.Controllers.Auth
 {
     public class CustomTokenManager : ICustomTokenManager
     {
+        private List<Token> tokens = new List<Token>();
+
         public string CreateToken(string username)
         {
+            var token = new Token(username);
+
+            tokens.Add(token);
+
             return string.Empty;
         }
 
         public bool VerifyToken(string token)
         {
-            return false;
+            return tokens.Any(x => token != null && token.Contains(x.TokenString) && x.ExpiryDate > System.DateTime.Now);
         }
 
-        public string GetUserInfoByToken(string token)
+        public string GetUserInfoByToken(string tokenString)
         {
+            var token = tokens.FirstOrDefault(x => tokenString != null && tokenString.Contains(x.TokenString));
+            if(token != null) 
+                return token.UserName;
+
             return string.Empty;
         }
     }
